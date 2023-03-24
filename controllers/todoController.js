72233cda19bd96153f64dd3359c89642594c13f2
Samuel Ige <samuelige.iyanu@gmail.com>
2023-03-24 04:00:12 +0100

    updateTodo: return a msg for an id that doesn't exist

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -43,7 +43,7 @@ const getTodo = async (req, res) => {
             
         }
 
-        return res.status(200).json({
+        res.status(200).json({
             data: queryResult
         });
     } catch (error) {
@@ -54,11 +54,18 @@ const getTodo = async (req, res) => {
 const updateTodo = async (req, res) => {
     try {
         await client.query('CREATE TABLE IF NOT EXISTS "todos" ("id" SERIAL PRIMARY KEY,"todo" varchar(100), created_on DATE NOT NULL);');
-        await client.query(`UPDATE todos SET todo = $1 WHERE id = $2`, [req.body.todo, req.params.id]);
 
-        return res.status(200).json({
-            message: `Updated a todo successfully`
-        });
+        const request = await client.query(`SELECT * from todos WHERE id = $1`, [req.params.id]);
+            if(request.rows.length) {
+                await client.query(`UPDATE todos SET todo = $1 WHERE id = $2`, [req.body.todo, req.params.id]);
+                return res.status(200).json({
+                    message: `Updated a todo successfully`
+                });
+            } else {
+                return res.status(400).json({
+                    msg: `No item found with id : ${req.params.id}`
+                });
+            }
     } catch (error) {
         next(error);
     }
